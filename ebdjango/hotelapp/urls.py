c1aa51e752331d79e50d00af69edc4fbb8731716
A00407059 <jongwon.shinn@smu.ca>
2022-03-10 17:16:39 -0400

    create read page get request

--- ebdjango/hotelapp/urls.py
+++ ebdjango/hotelapp/urls.py
@@ -6,5 +6,5 @@ urlpatterns=[
     path('', views.home, name="home"),
     path("hotel_list/", views.getListOfHotels, name="hotelList"),
     path('create/', views.create),
-    path('read/', views.read)
+    path('read/', views.getListOfHotels, name="hotelList")
 ]
--- ebdjango/hotelapp/views.py
+++ ebdjango/hotelapp/views.py
@@ -7,9 +7,27 @@ from rest_framework.response import Response
 from .models import Hotel
 from .serializers import HotelSerializers
 
+hotels = [
+{'id':1, 'name':'Holiday INN', 'price':'100', 'available':True},
+{'id':2, 'name':'Halifax Hotel', 'price':'120', 'available':True},
+{'id':3, 'name':'Canada Hotel', 'price':'130', 'available':True},
+{'id':4, 'name':'Marriet Hotel', 'price':'140', 'available':True}
 
+]
 def home(request):
-    return HttpResponse("Hello Jongwon Shinn")
+    return HttpResponse('''
+    <html>
+    <body>
+        <hi>Django RestAPI Assignment-Android<h1/>
+        <ol>
+            <li><a href="/read/">Hotel List</a></li>
+            <li><a href="/create/">Hotel Reservation</a></Li>
+        </ol>
+        
+    </body>
+    </html>   
+    
+    ''')
 
 def create(request):
     return HttpResponse("Create")
@@ -21,7 +39,7 @@ def read(request):
 @api_view(['GET', 'POST'])
 def getListOfHotels(request):
     if request.method == 'GET':
-        hotels_list = Hotel.objects.all()
-        hotelSerializer = HotelSerializers(hotels_list, many=True)
+
+        hotelSerializer = HotelSerializers(hotels, many=True)
 
         return Response(hotelSerializer.data)
