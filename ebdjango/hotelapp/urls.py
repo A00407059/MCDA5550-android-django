ea4a92b0ff275e2d3d6a1df1533fb85070dddb51
A00407059 <jongwon.shinn@smu.ca>
2022-03-10 15:08:15 -0400

    Migration and hotel_list view added

--- ebdjango/hotelapp/urls.py
+++ ebdjango/hotelapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns=[
 
-    path("home/", views.home, name="home")
+    path("home/", views.home, name="home"),
+    path("hotel_list/", views.getListOfHotels, name="hotelList")
 
 ]
--- ebdjango/hotelapp/views.py
+++ ebdjango/hotelapp/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Hotel
+from .serializers import HotelSerializers
+
 
 def home(request):
     return HttpResponse("Hello Jongwon Shinn")
+
+@api_view(['GET', 'POST'])
+def getListOfHotels(request):
+    if request.method == 'GET':
+        hotels_list = Hotel.objects.all()
+        hotelSerializer = HotelSerializers(hotels_list, many=True)
+
+        return Response(hotelSerializer.data)
